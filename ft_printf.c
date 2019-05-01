de356f2130e31fd798ff3d3dbc0597900662062f
goenka <estebanleclet@gmail.com>
2019-05-01 13:32:28 +0200

    sdds

--- ft_printf.c
+++ ft_printf.c
@@ -34,9 +34,9 @@ int		ft_printf(const char *format, ...)
 		{
 
 			str++;
-			if (*str == 'd')
+			if (*str == '%')
 			{
-				ft_putnbr(va_arg(arg, int));
+				ft_putchar('%');
 			}
 			if (*str == 'c')
 			{
@@ -46,16 +46,40 @@ int		ft_printf(const char *format, ...)
 			{
 				ft_putstr(va_arg(arg, char *));
 			}
-			if (*str == '%')
-			{
-				ft_putchar('%');
-			}
 			if (*str == 'p')
 			{
 				//unsigned int p = va_arg(arg, unsigned int);
 				//ft_putnbr(&p);
 				ft_putstr("pointer");
 			}
+			if (*str == 'd' || *str == 'i')
+			{
+				ft_putnbr(va_arg(arg, int));
+			}
+			if (*str == 'o') // Print octal
+			{
+				ft_putchar('i');
+			}
+			if (*str == 'u') // unsigned decimal
+			{
+				ft_putchar('i'); 
+			}
+			if (*str == 'x') // Hexa abcdef
+			{
+				ft_putchar('i');
+			}
+			if (*str == 'X') // hexa ABCDF
+			{
+				ft_putchar('i');
+			}
+			if (*str == '.') // precision
+			{
+				
+			}
+			if (*str == 'number') // Width
+			if (*str == '#') // precede octal by 0 or hex by 0x
+			if (*str == '0') // followed by number padding with 0 
+			
 		}
 		str++;
 		ft_putchar(*str);
@@ -73,4 +97,9 @@ int		ft_printf(const char *format, ...)
 
 	//printf("ALodaslkdas %d %d",1);
 	return (0);
+}
+
+int	parser(char *str)
+{
+	
 }
